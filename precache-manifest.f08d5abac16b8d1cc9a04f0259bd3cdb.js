b7a5c4460ac19fc824ec5f8b8c3987677c31bbcc
nejiko96 <nejiko2006@gmail.com>
2020-03-14 12:06:32 +0900

    Updates

---
+++ precache-manifest.f08d5abac16b8d1cc9a04f0259bd3cdb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa937260eadebd70d75198db094cc9e8",
+    "url": "/react-sir-like-model/index.html"
+  },
+  {
+    "revision": "3dd33a503399be3746bb",
+    "url": "/react-sir-like-model/static/js/2.af3a77bb.chunk.js"
+  },
+  {
+    "revision": "cb74030c757e88ed41d1616d3443d3e2",
+    "url": "/react-sir-like-model/static/js/2.af3a77bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74dffc0797b4d1b8dbe2",
+    "url": "/react-sir-like-model/static/js/main.87c5e026.chunk.js"
+  },
+  {
+    "revision": "a72bebcfa748c16160ab",
+    "url": "/react-sir-like-model/static/js/runtime-main.56155b09.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-sir-like-model/precache-manifest.c2ed5e2d68c6a920028d4101c2b4f4b6.js"
+  "/react-sir-like-model/precache-manifest.f08d5abac16b8d1cc9a04f0259bd3cdb.js"
 );
 
 self.addEventListener('message', (event) => {
